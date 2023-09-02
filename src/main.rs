9da3fcf5fcdcd1f9b84d6fba2edd78c1844f0cfb
JokNavi Mora <109341383+JokNavi@users.noreply.github.com>
2023-09-02 21:18:22 +0200

    showcase update

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
-use image::{io::Reader, imageops::FilterType};
+use image::{io::Reader, imageops::FilterType, DynamicImage};
 use rusttype::{Font, Scale};
 use crate::{average_key_brightnesses::{KeyBrightnesses, PRINTABLE_CHARACTERS}, image_to_keys::ImageToKeys};
 pub mod average_key_brightnesses;
 pub mod image_to_keys;
 
-fn custom_image_to_keys() {
+fn image_to_keys_custom(image: &mut DynamicImage) {
+    let font_bytes = include_bytes!("/home/joknavi/.local/share/fonts/RobotoMono-Regular.ttf");
+    let key_brightnesses = KeyBrightnesses::new(PRINTABLE_CHARACTERS, Font::try_from_bytes(font_bytes).unwrap(), Scale::uniform(30.0));
+    let keys = image.to_luma8().as_keys(&key_brightnesses).unwrap();
+    println!("{}", keys.join("\n"));
+}
+
+fn image_to_keys_default(image: &mut DynamicImage) {
+    let keys = image.to_luma8().as_keys(&KeyBrightnesses::default()).unwrap();
+    println!("{}", keys.join("\n"));
+}
+
+fn main() {
     let mut image = Reader::open("input/cool_cat.jpg")
             .unwrap()
             .decode()
@@ -16,13 +28,5 @@ fn custom_image_to_keys() {
         FilterType::Gaussian,
     );
     image = image.brighten(-60);
-    let font_bytes = include_bytes!("/home/joknavi/.local/share/fonts/RobotoMono-Regular.ttf");
-    let key_brightnesses = KeyBrightnesses::new(PRINTABLE_CHARACTERS, Font::try_from_bytes(font_bytes).unwrap(), Scale::uniform(30.0));
-    dbg!(&key_brightnesses.brightnesses());
-    let keys = image.to_luma8().as_keys(&key_brightnesses).unwrap();
-    println!("{}", keys.join("\n"));
-}
-
-fn main() {
-    custom_image_to_keys();
+    image_to_keys_default(&mut image);
 }
