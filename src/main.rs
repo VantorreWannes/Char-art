ddd1374426495743ceb5142f647513465619a7f9
JokNavi Mora <109341383+JokNavi@users.noreply.github.com>
2023-09-03 18:15:13 +0200

    Added clap and implemented a CLI interface
    Changes:
    - Added clap as a dependicy.
    - Added a CLI interface.

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,77 @@
-use image::{io::Reader, imageops::FilterType, DynamicImage};
-use rusttype::{Font, Scale};
+use std::fs;
+
 use crate::{average_key_brightnesses::KeyBrightnesses, image_to_keys::ImageToKeys};
+use clap::{arg, value_parser, Command};
+use image::{imageops::FilterType, io::Reader};
+use image_to_keys::keys_to_image;
+use rusttype::{Font, Scale};
 pub mod average_key_brightnesses;
 pub mod image_to_keys;
 
-const PRINTABLE_CHARACTERS: &str = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+fn main() -> Result<(), image::ImageError> {
+    let cmd = Command::new("key_art")
+        .args(&[
+            arg!(-p --path <Path> "File to be converted into keys.").required(true),
+            arg!(-s --shrink <u32> "Amount the file size will be divided by.")
+                .value_parser(value_parser!(u32)),
+            arg!(-d --darken <i32> "Amount to darken the image by.")
+                .value_parser(value_parser!(i32)),
+            arg!(-b --brighten <i32> "Amount to brighten the image by.")
+                .value_parser(value_parser!(i32)),
+        ])
+        .subcommand(Command::new("output").args(&[
+            arg!(-o --output <Path> "Output file.").required(true),
+            arg!(-f --font <Path> "Font file.").value_parser(value_parser!(String)),
+            arg!(-s --scale <f32> "Scale factor.").value_parser(value_parser!(f32)),
+        ]));
 
-#[allow(dead_code)]
-fn image_to_keys_custom(image: &mut DynamicImage) {
-    let font_bytes = include_bytes!("/home/joknavi/.local/share/fonts/RobotoMono-Regular.ttf");
-    let key_brightnesses = KeyBrightnesses::new(PRINTABLE_CHARACTERS, Font::try_from_bytes(font_bytes).unwrap(), Scale::uniform(30.0));
-    let keys = image.to_luma8().as_keys(&key_brightnesses).unwrap();
-    println!("{}", keys.join("\n"));
-}
+    let matches = cmd.get_matches();
+    let path = matches.get_one::<String>("path").unwrap();
+    let shrink = matches.get_one::<u32>("shrink");
+    let darken = matches.get_one::<i32>("darken");
+    let brighten = matches.get_one::<i32>("brighten");
 
-#[allow(dead_code)]
-fn image_to_keys_default(image: &mut DynamicImage) {
-    let keys = image.to_luma8().as_keys(&KeyBrightnesses::default()).unwrap();
-    println!("{}", keys.join("\n"));
-}
+    let mut image = Reader::open(path)?.decode()?;
+    if let Some(shrink) = shrink {
+        image = image.resize(
+            image.width() / shrink,
+            image.height() / shrink,
+            FilterType::Lanczos3,
+        );
+    }
+    if let Some(darken) = darken {
+        image = image.brighten(-darken);
+    }
+    if let Some(brighten) = brighten {
+        image = image.brighten(*brighten);
+    }
+    let keys = image.as_keys(&KeyBrightnesses::default()).unwrap();
 
+    if let Some(output) = matches.subcommand_matches("output") {
+        let output_path = output.get_one::<String>("output");
+        let font_path = output.get_one::<String>("font");
+        let scale = output.get_one::<f32>("scale");
 
-fn main() {
-    let mut image = Reader::open("input/skull.jpg")
-            .unwrap()
-            .decode()
-            .unwrap();
-    let downscale_amount = 4;
-    image = image.resize(
-        image.width() / downscale_amount,
-        image.height() / downscale_amount,
-        FilterType::Lanczos3,
-    );
-    image = image.brighten(-50);
-    image_to_keys_default(&mut image);
+        let font_bytes = match font_path {
+            Some(path) => fs::read(path)?,
+            None => {
+                include_bytes!("/home/joknavi/.local/share/fonts/RobotoMono-Regular.ttf").to_vec()
+            }
+        };
+        let scale = match scale {
+            Some(amount) => Scale::uniform(*amount),
+            None => Scale::uniform(12.0),
+        };
+        let key_image = keys_to_image(
+            &keys.as_slice(),
+            Font::try_from_bytes(&font_bytes).unwrap(),
+            scale,
+        )
+        .unwrap();
+        key_image.save(output_path.unwrap()).unwrap();
+    } else {
+        println!("{}", keys.join("\n"));
+    }
+
+    Ok(())
 }
